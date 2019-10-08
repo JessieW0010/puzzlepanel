bdee34d22040e4705821f84077eb8570232890b1
JessieW0010 <wangj.5415@gmail.com>
2019-10-08 15:40:07 -0400

    changed color state to be 2d array

--- App.js
+++ App.js
@@ -5,36 +5,33 @@ const width = Dimensions.get("screen").width;
 
 export default function App() {
 	const [tiles, setTiles] = useState([
-		"gray",
-		"gray",
-		"gray",
-		"gray",
-		"gray",
-		"gray",
-		"gray",
-		"gray",
-		"gray"
+		["gray", "gray", "gray"],
+		["gray", "gray", "gray"],
+		["gray", "gray", "gray"]
 	]);
+
+	const onPress = function() {};
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.grid}>
 				{/* Row 1 */}
 				<View style={[styles.row, { paddingTop: "2%" }]}>
-					<Tile color={tiles[0]} />
-					<Tile color={tiles[1]} />
-					<Tile color={tiles[2]} />
+					<Tile color={tiles[0][0]} />
+					<Tile color={tiles[0][1]} />
+					<Tile color={tiles[0][2]} />
 				</View>
 				{/* Row 2 */}
 				<View style={styles.row}>
-					<Tile color={tiles[3]} />
-					<Tile color={tiles[4]} />
-					<Tile color={tiles[5]} />
+					<Tile color={tiles[1][0]} />
+					<Tile color={tiles[1][1]} />
+					<Tile color={tiles[1][2]} />
 				</View>
 				{/* Row 3  */}
 				<View style={[styles.row, { paddingBottom: "2%" }]}>
-					<Tile color={tiles[6]} />
-					<Tile color={tiles[7]} />
-					<Tile color={tiles[8]} />
+					<Tile color={tiles[2][0]} />
+					<Tile color={tiles[2][1]} />
+					<Tile color={tiles[2][2]} />
 				</View>
 			</View>
 		</View>
