6d51dde81307a6718572a9c8878a3eb839d7b38c
JessieW0010 <wangj.5415@gmail.com>
2019-10-08 14:38:31 -0400

    created and styled simple 3x3 grid

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Dimensions,
+	TouchableOpacity
+} from "react-native";
 
 const width = Dimensions.get("screen").width;
 
@@ -7,7 +13,42 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.grid}>
-				<Text>Hi</Text>
+				{/* Row 1 */}
+				<View style={[styles.row, { paddingTop: "2%" }]}>
+					<TouchableOpacity style={styles.square}>
+						<Text>0</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>1</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>2</Text>
+					</TouchableOpacity>
+				</View>
+				{/* Row 2 */}
+				<View style={styles.row}>
+					<TouchableOpacity style={styles.square}>
+						<Text>3</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>4</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>5</Text>
+					</TouchableOpacity>
+				</View>
+				{/* Row 3  */}
+				<View style={[styles.row, { paddingBottom: "2%" }]}>
+					<TouchableOpacity style={styles.square}>
+						<Text>6</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>7</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.square}>
+						<Text>8</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		</View>
 	);
@@ -21,8 +62,20 @@ const styles = StyleSheet.create({
 		justifyContent: "center"
 	},
 	grid: {
-		backgroundColor: "green",
 		height: 0.9 * width,
-		width: 0.9 * width
+		width: 0.9 * width,
+		flexDirection: "column"
+	},
+	row: {
+		backgroundColor: "blue",
+		flexDirection: "row",
+		flex: 1,
+		paddingRight: "2%",
+		paddingLeft: "2%"
+	},
+	square: {
+		backgroundColor: "gray",
+		flex: 1,
+		margin: "2%"
 	}
 });
